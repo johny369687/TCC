17e78c96f4ac91b83753408e0e4ebb0a32febd4f
darkjoe234 <35602352+darkjoe234@users.noreply.github.com>
2018-05-05 03:08:37 -0300

    Update backend_teste.php

--- backend_teste.php
+++ backend_teste.php
@@ -3,31 +3,37 @@
 <title>Admin</title>
 
 <head>
+<link rel="stylesheet" type="text/css" href="cssbackend.css">
+
 </head>
 
 <body>
 
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="cssbackend.css">
-
+    
     <div class="underlay-photo"></div>
     <div class="underlay-black"></div>
     
-<form class="login-form" method="POST" action="PHP_login.php">
-        <p class="login-text">
-            <span class="fa-stack fa-lg">
-	      <i class="fa fa-circle fa-stack-2x"></i>
-	      <i class="fa fa-lock fa-stack-1x"></i>
-	    </span>
-        </p>
+    <div class="img-circle ">
 
+<img class="rounded-circle img-fluid" img-fluid src="icon.png" alt="">
+
+    <form class="login-form" method="POST" action="PHP_login.php">
+        
+       
+        
+
+
+
+       
+        </p>
+    
         <input type="username" class="login-username" autofocus="false" required="true" placeholder="Usuário" name="usuario" />
         <input type="password" class="login-password" required="true" placeholder="Senha"  name="senha"/>
         <input type="submit" name="Login" value="Login" class="login-submit"  />
-        </form>
+    
     </form>
-
-
+    </form>
 
 </body>
 </html>
